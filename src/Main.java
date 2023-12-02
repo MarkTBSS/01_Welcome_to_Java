abc0fcf73a6a891972ca1cfb722886e33e9f3ac5
MarkTBSS <marktbss@gmail.com>
2023-12-03 00:21:56 +0700

    [DELETE] .class file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 package src;
 
 public class Main {
+
     public static void main(String[] args) {
-        //System.out.println("Hello Java from Aiya");
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
+        // System.out.println("Hello Java from Aiya");
+        System.out.println("Hello, World from Main.");
+        System.out.println("Hello, Java from Main.");
     }
+
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,8 +1,10 @@
 package src;
 
-public class Solution {
+class Solution {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
+        System.out.println("Hello, World from Solution.");
+        System.out.println("Hello, Java from Solution.");
     }
+
 }
